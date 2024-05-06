c56f2252a6e89575a4a21195e4eef1bebaf8587f
patelsweta2 <sweta0442@gmail.com>
2024-05-06 20:34:48 +0530

    Complete videoDetails page and searchResult

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,12 +3,9 @@ import axios from "axios";
 const BASE_URL = "https://youtube138.p.rapidapi.com";
 
 const options = {
-  params: {
-    hl: "en",
-    gl: "US",
-  },
+  params: { hl: "en", gl: "US" },
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY,
+    "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
     "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
   },
 };
